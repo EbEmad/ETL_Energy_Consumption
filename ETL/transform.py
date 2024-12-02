08392a29280261ae1225ac8c35c71ad0ee31a7af
Ebrahim Emad <ebemad8@gmail.com>
2024-12-02 06:48:34 +0200

    new updates

--- ETL/transform.py
+++ ETL/transform.py
@@ -16,4 +16,11 @@ def transform_data(data):
         data_column_name:Consumption_data
     }
 
+    new_consumption_dataframe=pd.DataFrame(new_consumption_data)
+    data_clean=data_clean.drop(data_column_name,axis=1)
+
+    data_new=data_clean.join(new_consumption_dataframe)
+    return data_new
+
+
     
